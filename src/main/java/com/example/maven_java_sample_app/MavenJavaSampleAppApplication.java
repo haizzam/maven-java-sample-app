c17d9be79dba5818b61bc299c628d22fdd8163f8
haizzam <haizammohammed62@gmail.com>
2026-07-02 13:29:39 +0530

    Add files via upload

---
+++ src/main/java/com/example/maven_java_sample_app/MavenJavaSampleAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.maven_java_sample_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenJavaSampleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenJavaSampleAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/maven_java_sample_app/MavenJavaSampleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maven_java_sample_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenJavaSampleAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
